d725f18b2af96d70553c4806db0a168fa84e91db
DaffaKhaizaIrawan <168259009+DaffaKhaizaIrawan@users.noreply.github.com>
2024-05-13 20:40:10 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const Posts = () => import("../views/Posts.vue");
+const Post = () => import("../views/Posts.vue");
 
 const routes = [
-    { path: "/", component: Posts},
-    { path: "/todos", component: () => import("../views/Todos.vue")}
+    { path: "/", component: Post},
+    { path: "/todo", component: () => import("../views/Todos.vue")}
 ];
 
 const router = createRouter({
